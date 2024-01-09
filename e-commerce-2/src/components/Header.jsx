ccd78e15e56bdb07d6504574a84e9fca66725d01
Belen Ramos <m.b.ramos0204@gmail.com>
2024-01-09 09:28:53 -0300

    Header

---
+++ e-commerce-2/src/components/Header.jsx
@@ -0,0 +1,34 @@
+const Header = () => {
+    return(
+        <div className="conteiner">
+            <div className="row">
+                <div className="col">
+                    <img src="" alt="" />
+                </div>
+                <div className="col">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link"  href="#">Summer Looks</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Coleccion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Beauty & Spa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gift Card</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col">
+                    <a href="" className="btn btn-warning">Crea una cuenta</a>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default Header;
